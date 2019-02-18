4886de0d3d1336a0a142c4ef9c389ab5a28f3580
Binod <chaku.lama@gmail.com>
2019-02-19 00:34:48 +0100

    mobile working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  nombre:string;
+  password:string;
+
+  constructor (public navCtrl : NavController){}
+
+
+  Autentificar()
+  {console.log(" login successful:"+this.nombre);
+  console.log(" Password:"+this.password);}
+
+  Greet()
+{
+  console.log("hola");
+}
 
 }
