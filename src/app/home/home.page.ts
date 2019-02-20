7d6586646b253e21468d1836eb662e16b6fdf152
Binod <chaku.lama@gmail.com>
2019-02-20 21:12:19 +0100

    log in works

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,14 +10,30 @@ import { NavController } from '@ionic/angular';
 export class HomePage {
   nombre:string;
   password:string;
+  private APIUrl="http://127.0.0.1:3000/personas";
 
-  constructor (public navCtrl : NavController){}
+  constructor (public navCtrl : NavController,
+    private http:HttpClient){}
 
 
   Autentificar()
-  {console.log(" login successful:"+this.nombre);
-  console.log(" Password:"+this.password);}
-
+  {
+    this.http.get<any>(this.APIUrl+'/'+this.nombre).subscribe(
+                                                  persona=> {
+                                                    if (persona != null)
+                                                    {
+                                                      if (persona.rol ==="Profesor")
+                                                        console.log("Profesor")
+                                                                                                               
+                                                      else{
+                                                          //window.location.href='./alumno/'+persona.nombre;
+                                                          console.log("Log in "+persona.nombre );
+                                                          alert("Log in "+persona.nombre);
+                                                        }
+                                                    }
+                                                   
+                                                      } );
+   }
   Greet()
 {
   console.log("hola");
